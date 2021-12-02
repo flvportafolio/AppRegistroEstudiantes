1307d6faa9019a6922373d16d1df6cc57e5f3072
fabiolv2020 <fabiolv2019@hotmail.com>
2021-12-02 18:59:50 -0400

    se creo un proyecto de UnitTest en la solucion y se creo una prueba de ejemplo usando selenium, tambien se instalo por nuget selenium.webdriver, selenium.support y selenium.webdriver.chromedriver

---
+++ TrainingUnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace TrainingUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSearchGoogle()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+            IWebElement txtBuscar = driver.FindElement(By.CssSelector(".gLFyf[title='Buscar']"));
+            txtBuscar.SendKeys("Selenium");
+            var txtBuscarValue = txtBuscar.GetAttribute("value");
+            txtBuscar.Click();
+            Assert.IsTrue(txtBuscarValue == "Selemium", "Erro en el input");
+
+        }
+    }
+}
